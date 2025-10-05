e0775fa8af449d4a0b55207315ab99ebedcf827d
jeongo01 <wjddh123456@gmail.com>
2025-10-05 17:48:49 +0900

    JDBCInsertTest, DB

---
+++ src/com/koreaIT/example/JAM/JDBCSelectTest.java
@@ -0,0 +1,78 @@
+package com.koreaIT.example.JAM;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.koreaIT.example.JAM.dto.Article;
+
+public class JDBCSelectTest {
+	public static void main(String[] args) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		List<Article> articles = new ArrayList<>();
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://127.0.0.1:3306/JAM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+			conn = DriverManager.getConnection(url, "root", "");
+
+			String sql = "SELECT * FROM article";
+			sql += "ORDER BY id DESC;";
+			
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery(sql);
+			
+			while(rs.next()) {
+				int id = rs.getInt("id");
+				String regDate = rs.getString("regDate");
+				String updateDate = rs.getString("updateDate");
+				String title = rs.getString("title");
+				String body = rs.getString("body");
+				
+				Article article = new Article(id, regDate, updateDate, title, body);
+				articles.add(article);
+				
+			}
+			
+			for(Article article : articles) {
+				System.out.printf("%d, %s, %s, %s, %s\n", article.id, article.regDate, article.updateDate, article.title, article.body);
+			}
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패");
+		} catch (SQLException e) {
+			System.out.println("에러: " + e);
+		} finally {
+			try {
+				if(rs != null && !rs.isClosed()) {
+					rs.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+			try {
+				if (pstmt != null && !pstmt.isClosed()) {
+					pstmt.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			try {
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
--- src/com/koreaIT/example/JAM/dto/Article.java
+++ src/com/koreaIT/example/JAM/dto/Article.java
@@ -1,7 +1,10 @@
 package com.koreaIT.example.JAM.dto;
-public class Article {
 
+public class Article {
+	// 필드
 	public int id;
+	public String regDate;
+	public String updateDate;
 	public String title;
 	public String body;
 	
@@ -11,4 +14,14 @@ public class Article {
 		this.body = body;
 	}
 	
+	// 생성자 역할 : DB에서 받아온 인자(실제값)를 받아서 객체 필드에 저장하는 과정. 
+	public Article(int id, String regDate, String updateDate, String title, String body) {   // (생성자의 선언부) 파라미터 : id, title, body (DB에서 가져온 값을 받는 변수)
+		this.id = id;    // 인자로 받아온 데이터를 파라미터 저장하고, 파라미터 id가 필드 this.id 에 저장된다.  
+		this.regDate = regDate;
+		this.updateDate = updateDate;
+		this.title = title;
+		this.body = body;
+	}
+	// 애초에 new Article(...)을 호출하면, Article 객체가 힙 메모리에 생성되고, 그 안의 필드(id, regDate, updateDate, 등등)도 초기화된다.
+	// 초기화된 필드들은 기본값을 가진다. id = 0, String(참조형) 타입인 나머지들은 Null을 갖는다.  
 }
