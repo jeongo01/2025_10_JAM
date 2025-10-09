738d733aed5f02317c813879250de213f10e0266
jeongo01 <wjddh123456@gmail.com>
2025-10-09 17:56:29 +0900

    article list에 DBUtil 적용

--- src/com/koreaIT/example/JAM/App.java
+++ src/com/koreaIT/example/JAM/App.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 import com.koreaIT.example.JAM.dto.Article;
@@ -81,44 +82,54 @@ public class App {
 					System.out.println("== 게시물 목록 ==");
 
 					List<Article> articles = new ArrayList<>();
-
-					try {
-						String sql = "SELECT * FROM article";
-						sql += " ORDER BY id DESC;";
-
-						pstmt = conn.prepareStatement(sql);
-						rs = pstmt.executeQuery(sql);
-
-						while (rs.next()) {
-							int id = rs.getInt("id");
-							String regDate = rs.getString("regDate");
-							String updateDate = rs.getString("updateDate");
-							String title = rs.getString("title");
-							String body = rs.getString("body");
-
-							Article article = new Article(id, regDate, updateDate, title, body);
-							articles.add(article);
-
-						}
-
-						for (Article article : articles) {
-							System.out.printf("%d, %s, %s, %s, %s\n", article.id, article.regDate, article.updateDate,
-									article.title, article.body);
-						}
-
-					} catch (SQLException e) {
-						System.out.println("에러: " + e);
+					
+					SecSql sql = SecSql.from("SELECT * FROM article");
+					sql.append("ORDER BY id DESC");
+					
+					List<Map<String, Object>> articleListMap = DBUtil.selectRows(conn, sql);
+					
+					for (Map<String, Object> articleMap : articleListMap) {
+						Article article = new Article(articleMap);
+						articles.add(article);
 					}
+
+//					try {
+//						String sql = "SELECT * FROM article";
+//						sql += " ORDER BY id DESC;";
+//
+//						pstmt = conn.prepareStatement(sql);
+//						rs = pstmt.executeQuery(sql);
+//
+//						while (rs.next()) {
+//							int id = rs.getInt("id");
+//							String regDate = rs.getString("regDate");
+//							String updateDate = rs.getString("updateDate");
+//							String title = rs.getString("title");
+//							String body = rs.getString("body");
+//
+//							Article article = new Article(id, regDate, updateDate, title, body);
+//							articles.add(article);
+//
+//						}
+//
+//						for (Article article : articles) {
+//							System.out.printf("%d, %s, %s, %s, %s\n", article.id, article.regDate, article.updateDate,
+//									article.title, article.body);
+//						}
+//
+//					} catch (SQLException e) {
+//						System.out.println("에러: " + e);
+//					}
 
 					if (articles.size() == 0) {
 						System.out.println("존재하는 게시물이 없습니다");
 						continue;
 					}
 
-					System.out.println("번호	|	제목");
+					System.out.println("번호	|	 제목	|	날짜");
 
 					for (Article article : articles) {
-						System.out.printf("%d	|	%s\n", article.id, article.title);
+						System.out.printf("%d	|	%s	|	%s\n", article.id, article.title, article.regDate);
 					}
 				}
 				
--- src/com/koreaIT/example/JAM/dto/Article.java
+++ src/com/koreaIT/example/JAM/dto/Article.java
@@ -1,10 +1,13 @@
 package com.koreaIT.example.JAM.dto;
 
+import java.time.LocalDateTime;
+import java.util.Map;
+
 public class Article {
 	// 필드
 	public int id;
-	public String regDate;
-	public String updateDate;
+	public LocalDateTime regDate;
+	public LocalDateTime updateDate;
 	public String title;
 	public String body;
 	
@@ -15,7 +18,7 @@ public class Article {
 	}
 	
 	// 생성자 역할 : DB에서 받아온 인자(실제값)를 받아서 객체 필드에 저장하는 과정. 
-	public Article(int id, String regDate, String updateDate, String title, String body) {   // (생성자의 선언부) 파라미터 : id, title, body (DB에서 가져온 값을 받는 변수)
+	public Article(int id, LocalDateTime regDate, LocalDateTime updateDate, String title, String body) {   // (생성자의 선언부) 파라미터 : id, title, body (DB에서 가져온 값을 받는 변수)
 		this.id = id;    // 인자로 받아온 데이터를 파라미터 저장하고, 파라미터 id가 필드 this.id 에 저장된다.  
 		this.regDate = regDate;
 		this.updateDate = updateDate;
@@ -23,5 +26,13 @@ public class Article {
 		this.body = body;
 	}
 	// 애초에 new Article(...)을 호출하면, Article 객체가 힙 메모리에 생성되고, 그 안의 필드(id, regDate, updateDate, 등등)도 초기화된다.
-	// 초기화된 필드들은 기본값을 가진다. id = 0, String(참조형) 타입인 나머지들은 Null을 갖는다.  
+	// 초기화된 필드들은 기본값을 가진다. id = 0, String(참조형) 타입인 나머지들은 Null을 갖는다.
+	
+	public Article(Map<String, Object> articleMap) {
+		this.id = (int) articleMap.get("id");  
+		this.regDate = (LocalDateTime) articleMap.get("regDate");
+		this.updateDate = (LocalDateTime) articleMap.get("updateDate");
+		this.title = (String) articleMap.get("title");
+		this.body = (String) articleMap.get("body");
+	}
 }
