1940758c13f5e5e177ffe6b5606daf211ee9c2d1
jeongo01 <wjddh123456@gmail.com>
2025-10-29 21:01:25 +0900

    ArticleController, ArticleService, ArticleDao

--- src/com/koreaIT/example/JAM/App.java
+++ src/com/koreaIT/example/JAM/App.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
+import com.koreaIT.example.JAM.controller.ArticleController;
 import com.koreaIT.example.JAM.controller.MemberController;
 import com.koreaIT.example.JAM.dto.Article;
 import com.koreaIT.example.JAM.util.DBUtil;
@@ -29,6 +30,7 @@ public class App {
 			conn = DriverManager.getConnection(url, "root", "");
 			
 			MemberController memberController = new MemberController(conn, sc);
+			ArticleController articleController = new ArticleController(conn, sc);
 
 			while (true) {
 				System.out.printf("명령어) ");
@@ -43,133 +45,23 @@ public class App {
 				}
 
 				else if (cmd.equals("article write")) {
-
-					System.out.println("== 게시물 작성 ==");
-
-					System.out.printf("제목 : ");
-					String title = sc.nextLine().trim();
-					System.out.printf("내용 : ");
-					String body = sc.nextLine().trim();
-
-					SecSql sql = SecSql.from("INSERT INTO article");
-					sql.append("SET regDate = NOW()");
-					sql.append(", updateDate = NOW()");
-					sql.append(", title = ?", title);
-					sql.append(", `body` = ?", body);
-
-					int id = DBUtil.insert(conn, sql);
-
-					System.out.printf("%d번 게시물이 생성되었습니다\n", id);
-
+					articleController.doWrite();
 				}
 
 				else if (cmd.equals("article list")) {
-
-					System.out.println("== 게시물 목록 ==");
-
-					List<Article> articles = new ArrayList<>();
-
-					SecSql sql = SecSql.from("SELECT * FROM article");
-					sql.append("ORDER BY id DESC");
-
-					List<Map<String, Object>> articleListMap = DBUtil.selectRows(conn, sql);
-
-					for (Map<String, Object> articleMap : articleListMap) {
-						Article article = new Article(articleMap);
-						articles.add(article);
-					}
-
-					if (articles.size() == 0) {
-						System.out.println("존재하는 게시물이 없습니다");
-						continue;
-					}
-
-					System.out.println("번호	|	 제목	|	날짜");
-
-					for (Article article : articles) {
-						System.out.printf("%d	|	%s	|	%s\n", article.id, article.title, article.regDate);
-					}
+					articleController.showList();
 				}
 
 				else if (cmd.startsWith("article detail ")) {
-
-					int id = Integer.parseInt(cmd.split(" ")[2]);
-
-					SecSql sql = SecSql.from("SELECT * FROM article");
-					sql.append("WHERE id = ?", id);
-
-					Map<String, Object> articleMap = DBUtil.selectRow(conn, sql);
-
-					if (articleMap.isEmpty()) {
-						System.out.printf("%d번 게시물이 존재하지 않습니다.\n", id);
-						continue;
-					}
-
-					Article article = new Article(articleMap);
-
-					System.out.printf(" == %d번 게시물 상세보기 == \n", id);
-
-					System.out.printf("번호 : %d\n", article.id);
-					System.out.printf("작성일 : %s\n", article.regDate);
-					System.out.printf("수정일 : %s\n", article.updateDate);
-					System.out.printf("제목 : %s\n", article.title);
-					System.out.printf("내용 : %s\n", article.body);
-
+					articleController.showDetail(cmd);
 				}
 
 				else if (cmd.startsWith("article modify ")) {
-
-					int id = Integer.parseInt(cmd.split(" ")[2]);
-
-					SecSql sql = SecSql.from("SELECT COUNT(*) FROM article");
-					sql.append("WHERE id = ?", id);
-
-					int articleCnt = DBUtil.selectRowIntValue(conn, sql);
-
-					if (articleCnt == 0) {
-						System.out.printf("%d번 게시물이 존재하지 않습니다.\n", id);
-						continue;
-					}
-
-					System.out.println(" ==게시물 수정== ");
-
-					System.out.printf("수정할 제목 : ");
-					String title = sc.nextLine().trim();
-					System.out.printf("수정할 내용 : ");
-					String body = sc.nextLine().trim();
-
-					sql = SecSql.from("UPDATE article");
-					sql.append("SET updateDate = NOW()");
-					sql.append(", title = ?", title);
-					sql.append(", `body` = ?", body);
-					sql.append("WHERE id = ?", id);
-
-					DBUtil.update(conn, sql);
-
-					System.out.printf("%d번 게시물이 수정되었습니다.\n", id);
+					articleController.doModify(cmd);
 				}
 
 				else if (cmd.startsWith("article delete ")) {
-
-					int id = Integer.parseInt(cmd.split(" ")[2]);
-
-					SecSql sql = SecSql.from("SELECT COUNT(*) FROM article");
-					sql.append("WHERE id = ?", id);
-
-					int articleCnt = DBUtil.selectRowIntValue(conn, sql);
-
-					if (articleCnt == 0) {
-						System.out.printf("%d번 게시물이 존재하지 않습니다.\n", id);
-						continue;
-					}
-
-					sql = SecSql.from("DELETE FROM article");
-					sql.append("WHERE id = ?", id);
-
-					DBUtil.delete(conn, sql);
-
-					System.out.printf("%d번 게시물이 삭제되었습니다.\n", id);
-
+					articleController.doDelete(cmd);
 				} 
 				else {
 					System.out.println("존재하지 않는 명령어 입니다.");
---
+++ src/com/koreaIT/example/JAM/service/ArticleService.java
@@ -0,0 +1,62 @@
+package com.koreaIT.example.JAM.service;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.koreaIT.example.JAM.dao.ArticleDao;
+import com.koreaIT.example.JAM.dto.Article;
+
+public class ArticleService {
+
+	private ArticleDao articleDao;
+
+	public ArticleService(Connection conn) {
+		this.articleDao = new ArticleDao(conn);
+	}
+
+	public int doWrite(String title, String body) {
+		return articleDao.doWrite(title, body);
+	}
+
+	public List<Article> showList() {
+		
+		List<Map<String, Object>> articleListMap = articleDao.showList();
+		
+		List<Article> articles = new ArrayList<>();
+
+		for (Map<String, Object> articleMap : articleListMap) {
+			articles.add(new Article(articleMap));
+		}
+		
+		return articles;
+	}
+	
+	public int getNumInCmd(String cmd) {   // 사용자가 입력한 명령어에서 숫자(id) 뽑기
+		return Integer.parseInt(cmd.split(" ")[2]);
+	}
+	
+	public int getArticleCnt(int id) {  // 게시물이 존재하는지 파악
+		return articleDao.getArticleCnt(id);
+	}
+
+	public Article showDetail(int id) {
+		
+		Map<String, Object> articleMap = articleDao.showDetail(id);
+		
+		if(articleMap.isEmpty()) {
+			return null;
+		}
+		
+		return new Article(articleMap);
+	}
+
+	public void doModify(String title, String body, int id) {
+		articleDao.doModify(title, body, id);
+	}
+
+	public void doDelete(int id) {
+		articleDao.doDelete(id);
+	}
+}
